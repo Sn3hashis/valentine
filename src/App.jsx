9f8560f48c98a82695ea169aeb96a73024c885d9
Snehashis Mukherjee <77873741+Sn3hashis@users.noreply.github.com>
2024-02-14 19:11:50 +0530

    Update App.jsx
    Added her name

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,8 @@ export default function Page() {
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
           <div className="text-4xl md:text-6xl font-bold my-4">
-            Ok Yayyyyy!!!
+            Yayyyyy!!!
+            I ❤️ You Jaan, My Montaaassss ❤️
           </div>
         </>
       ) : (
@@ -61,7 +62,8 @@ export default function Page() {
             src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+            Will you be my Valentine Mona?
+            Please come to your Daddy Arms Love ❤️😘
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
